94f7702d65257e6194f6f90c4284c531eef5ab62
MatiBiela <mateuszbiela9@gmail.com>
2021-02-01 12:01:07 +0100

    62.1

---
+++ 62.1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	
+	int liczba;
+	int t[1000];
+	//int max[1000];
+	int minimum =0;
+	int maximum =1000;
+	int i;
+	ifstream plik;
+	
+	plik.open("liczby1.txt");
+	if(plik.good()){
+    while(!plik.eof()){
+		plik >> oct >> t[i];
+		{
+		if(minimum < t[i])
+		minimum = t[i];
+		cout << oct <<"Najwieksza liczba: "<< minimum << endl; 
+	}
+	{
+		if(maximum > t[i])
+		maximum = t[i];
+		cout << oct <<"Najwieksza liczba: "<<  maximum;
+	}
+		
+}
+	
+	
+}
+	
+	
+	plik.close();
+	return 0;
+}
+
